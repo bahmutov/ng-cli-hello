e609c09cdad713b29f335edc348f5f28719bf15d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-05 22:46:57 -0500

    add po and links test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,3 +1,13 @@
-it('loads', () => {
-  cy.visit('http://localhost:4200')
-})
+import { navigateTo, getGreeting } from '../support/po';
+
+describe('Hello Angular', () => {
+  beforeEach(navigateTo);
+
+  it('should display welcome message', () => {
+    getGreeting().contains('Welcome to app!');
+  });
+
+  it('has 3 links', () => {
+    cy.get('app-root li a').should('have.length', 3);
+  });
+});
